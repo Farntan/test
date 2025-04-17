b3bf4548459531cd31236cedb6fbe7df5a83facb
Farntan <gidrotgv@yandex.ru>
2025-04-17 16:34:18 +0400

    Second

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,10 +1,16 @@
 <?php
 use PHPUnit\Framework\TestCase;
+use classes\config;
 
 class ExampleTest extends TestCase
 {
     public function testAddition()
     {
+        $this->assertEquals(4, 2 + 2);
+    }
+    public function createConfig()
+    {
+        $config=new Config;
         $this->assertEquals(4, 2 + 2);
     }
 }
