605b363303722145b8f44bebc71af5625bbe21fc
Farntan <gidrotgv@yandex.ru>
2025-04-18 15:24:46 +0400

    1. Исправление правил валидации inn физического лица input_inn.php.

--- view/reports/natural_person/input_inn.php
+++ view/reports/natural_person/input_inn.php
@@ -3,6 +3,6 @@
 return '
 <div class="mb-3">
   <label for="inn" class="form-label">ИНН</label>
-  <input type="number" class="form-control" id="inn" name="inn" placeholder="ИНН" required maxlength=12" minlength="12">
+  <input type="text" class="form-control" id="inn" name="inn" placeholder="ИНН" required maxlength=12" minlength="12" pattern="[0-9]{12}">
 </div>
 ';
