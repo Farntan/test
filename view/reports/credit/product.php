6c1c495b1c28fdbdc429b18f56d0b704a8f2701c
Farntan <gidrotgv@yandex.ru>
2025-04-21 16:12:47 +0400

    add commentary

--- view/reports/credit/product.php
+++ view/reports/credit/product.php
@@ -3,5 +3,5 @@
     $deposit_period=include ('input_credit_period.php');
     $payment_schedule=include ('select_payment_schedule.php');
     $deposit_amount=include ('input_credit_amount.php');
-
-    return $date_open_close.$deposit_period.$payment_schedule.$deposit_amount;
+    $titleProduct="<h5  class='form-label'>Данные продукта:</h5>";
+    return $titleProduct.$date_open_close.$deposit_period.$payment_schedule.$deposit_amount;
--- view/reports/deposit/input_date_open_close.php
+++ view/reports/deposit/input_date_open_close.php
@@ -2,12 +2,23 @@
 $min_date=date('Y-m-d');
 
 return '
-<label for="date_open" class="form-label">Дата открытия/закрытия</label>
-<div class="input-group mb-3" id="date_open">
-   
-    <input type="date" class="form-control me-2" placeholder="дата открытия"  name="date_open" aria-label="дата открытия" min="' . $min_date . '" required >
-    <input type="date" class="form-control" placeholder="дата закрытия"  name="date_close" aria-label="дата закрытия" min="' . $min_date . '" required >
 
-  
+<div class="row">
+<div class="col-6">
+ <label for="date_open" class="form-label">Дата открытия</label>
+<div class="input-group mb-3" id="date_open">
+      
+        <input type="date" class="form-control me-2" placeholder="дата открытия"  name="date_open" aria-label="дата открытия" min="' . $min_date . '" required >
+     </div>  
 </div>
+<div class="col-6">
+<label for="date_close" class="form-label">Дата закрытия</label>
+  <div class="input-group mb-3" id="date_open"> 
+       
+        <input type="date" class="form-control" placeholder="дата закрытия"  name="date_close" aria-label="дата закрытия" min="' . $min_date . '" required > 
+    </div>
+</div>   
+     
+</div>
+
 ';
